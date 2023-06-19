7fe03cbfaee0e4e0f24f9526d99218e51156ffe4
Faevoh <favourigbani29@gmail.com>
2023-06-19 13:55:52 +0100

    envChanges

--- db/data-source.ts
+++ db/data-source.ts
@@ -10,11 +10,11 @@ config();
 
 export const dataSourceOptions: DataSourceOptions = {
     type: "mysql",
-    host: "localhost",
-    port: 3306,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "money_App",
+    database: process.env.DATABASE,
     entities: [User, Compliances, Airtime, Wallet, Transactions ],
     synchronize: false,
     migrations: ["dist/db/migrations/*.js"]
