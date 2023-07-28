cf6256708438a99f29fdb8e0c55603c63ea12966
Faevoh <favourigbani29@gmail.com>
2023-07-28 14:18:59 +0100

    new commit

--- dist/src/DTO/createUser.d.ts
+++ dist/src/DTO/createUser.d.ts
@@ -3,5 +3,5 @@ export declare class CreateUserDto {
     lastName: string;
     email: string;
     password: string;
-    accountType: 'business' | 'personal';
+    accountType: string;
 }
--- src/DTO/createUser.ts
+++ src/DTO/createUser.ts
@@ -20,5 +20,5 @@ export class CreateUserDto {
     password: string;
 
     @IsNotEmpty()
-    accountType: 'business' | 'personal';
+    accountType: string;
 }
