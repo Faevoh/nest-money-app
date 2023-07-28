68caea9e41acccb1c5ec9d92e27ad7f40adf713e
Faevoh <favourigbani29@gmail.com>
2023-07-28 02:41:04 +0100

    Changes made in wallet.module

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -3,11 +3,12 @@ import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Wallet } from 'src/Entities/walletEntity.entity';
+import { accountGenerator } from 'src/auth/generator.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Wallet])],
   controllers: [WalletController],
-  providers: [WalletService],
+  providers: [WalletService, accountGenerator],
   exports: [WalletService]
 })
 export class WalletModule {}
