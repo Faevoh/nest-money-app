c286ac6f5e5c02d30d0d81314a02f8b7b5494132
Faevoh <favourigbani29@gmail.com>
2023-08-03 14:52:32 +0100

    changes in transaction.controller

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -33,9 +33,9 @@ export class TransactionController {
         const walletdata = await this.walletService.findByUserId(userId)
         console.log(walletdata)
 
-        // walletdata.accountBalance += transaction.amount
-        // console.log("transAmount",transaction.amount)
-        // console.log("wallBal",walletdata.accountBalance)
+        walletdata.accountBalance += transaction.amount
+        console.log("transAmount",transaction.amount)
+        console.log("wallBal",walletdata.accountBalance)
 
         // const savedWallet = await this.walletService.saveWallet(walletdata)
         // console.log(savedWallet)
