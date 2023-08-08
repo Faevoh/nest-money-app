18eb81e609226d4ff0fa5d53ecd29879f94b5118
Faevoh <favourigbani29@gmail.com>
2023-08-08 16:32:25 +0100

    Made changes in user.controller

--- dist/src/user/user.controller.js
+++ dist/src/user/user.controller.js
@@ -259,8 +259,10 @@ let UserController = class UserController {
             }
             const id = tokenDecode.sub;
             const user = await this.userService.findById(id);
-            const data = await this.bankPinservice.createPin(user, userPinDto);
-            if (data.bankPin !== null) {
+            if (user.bankPin == null) {
+                const data = await this.bankPinservice.createPin(user, userPinDto);
+            }
+            else {
                 throw new common_1.BadRequestException("You already have a Pin");
             }
             return { statusCode: 201, message: "Pin created" };
