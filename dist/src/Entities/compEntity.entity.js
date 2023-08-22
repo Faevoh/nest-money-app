b852e4bbc7e80fa1e538aa92cd8b6e53b2c387b0
Faevoh <favourigbani29@gmail.com>
2023-08-22 13:17:28 +0100

    Added changes in compliance

--- dist/src/Entities/compEntity.entity.js
+++ dist/src/Entities/compEntity.entity.js
@@ -23,7 +23,7 @@ __decorate([
     __metadata("design:type", String)
 ], Compliances.prototype, "BVN", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ unique: true }),
+    (0, typeorm_1.Column)({ unique: true, nullable: true }),
     __metadata("design:type", String)
 ], Compliances.prototype, "NIN", void 0);
 __decorate([
